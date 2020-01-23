630b5e5eea1789244ba5646fc32425d00508d28f
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-01-23 15:13:25 -0800

    overhauled the way the program works removed pygame and put the qr in the console removed all logging except for print() and the QR

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from app import app
-import subprocess
+import qr
+import io, sys
 
 
 if __name__ == '__main__':
-    x = subprocess.Popen("python pg.py " + app.config["QR_IP"] + app.config["KEY"], shell=True)
+    #create a QR code and print to console
+    qr.createQR(app.config["QR_IP"] + app.config["KEY"], True)
+
+    #disable text legging from app
+    text_trap = io.StringIO()
+    sys.stdout = text_trap
+
     app.run(host="0.0.0.0", port=app.config["PORT"])
