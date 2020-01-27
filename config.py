7350dfe2c6621f8f71fbc647ead47ae943701c9f
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-01-26 19:25:59 -0800

    update to config to support upload and download

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import os
 from random import randint
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '&PkGWqfp?kFE9TDLw-URW=-P2tVou4QyMH4tJz2LP~BNGA!B7-siyfT.&inE:@*@' # Prevents CSRF attacks
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET KEY' # Prevents CSRF attacks
     UPLOAD_FOLDER = "E:/fileShare/files"                                                                            # Where you want files to be downloaded to
-    KEY = str(randint(0,1000000000000))                                                                             # random key to prevent access through guessing port
+    DOWNKEY = str(randint(0,1000000000000))                                                                         # random key to prevent access through guessing port (for download)
+    UPKEY = str(randint(0,1000000000000))                                                                           # random key to prevent access through guessing port (for upload)
     PORT = 8787                                                                                                     # port to run server on
-    QR_IP = "http://PUBLIC_IP:" + str(PORT) +"/"                                                                    # IP adress + port to display on qr
+    QR_IP = "http://IP_ADDRESS:" + str(PORT) +"/"                                                                   # IP adress + port to display on qr
+    ROOT = os.path.dirname(os.path.abspath(__file__))
